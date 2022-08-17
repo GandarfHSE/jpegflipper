915bf2de4be4eb2ac8c46e5e20e927c27ac9185f
Valentin Ilyushin <gandarfgamer200219@gmail.com>
2022-08-17 15:32:36 +0300

    Fix possible segfault and add exception handling in callback

--- task.cpp
+++ task.cpp
@@ -1,3 +1,6 @@
+#include <exception>
+#include <memory>
+
 #include <evhttp.h>
 #include <Magick++.h>
 
@@ -5,29 +8,37 @@
 
 // Do generic callback
 void CbTask::operator() () {
-    evbuffer* inBuf = evhttp_request_get_input_buffer(request);
-    evbuffer* outBuf = evhttp_request_get_output_buffer(request);
+    try {
+        evbuffer* inBuf = evhttp_request_get_input_buffer(request);
+        evbuffer* outBuf = evhttp_request_get_output_buffer(request);
 
-    size_t imgLen = evbuffer_get_length(inBuf);
-    char buf[imgLen + 3];
+        size_t imgLen = evbuffer_get_length(inBuf);
+        std::unique_ptr<char[]> bufPtr(new char[imgLen + 3]);
 
-    size_t readTotal = 0;
-    while (evbuffer_get_length(inBuf)) {
-        int readNow = evbuffer_remove(inBuf, &buf[readTotal], imgLen);
-        if (readNow > 0) {
-            readTotal += readNow;
+        size_t readTotal = 0;
+        while (evbuffer_get_length(inBuf)) {
+            int readNow = evbuffer_remove(inBuf, &(bufPtr.get())[readTotal], imgLen);
+            if (readNow > 0) {
+                readTotal += readNow;
+            }
         }
+
+        // work with image
+        Magick::Blob blob(static_cast<void*>(bufPtr.get()), imgLen + 1);
+        Magick::Image image(blob);
+        image.flop();
+        image.write(&blob);
+
+        if (evbuffer_add(outBuf, blob.data(), blob.length()) == -1) {
+            evhttp_send_reply(request, HTTP_INTERNAL, "copy error occured", nullptr);
+        }
+
+        evhttp_send_reply(request, HTTP_OK, "", outBuf);
     }
-
-    // work with image
-    Magick::Blob blob((void*)buf, imgLen + 1);
-    Magick::Image image(blob);
-    image.flop();
-    image.write(&blob);
-
-    if (evbuffer_add(outBuf, blob.data(), blob.length()) == -1) {
-        evhttp_send_reply(request, HTTP_INTERNAL, "copy error occured", nullptr);
+    catch (std::exception& e) {
+        evhttp_send_reply(request, HTTP_BADREQUEST, e.what(), nullptr);
     }
-
-    evhttp_send_reply(request, HTTP_OK, "", outBuf);
+    catch (...) {
+        evhttp_send_reply(request, HTTP_BADREQUEST, "some error occured", nullptr);
+    }
 }
